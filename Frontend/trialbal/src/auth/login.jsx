b7c22dab30b715e18a27445623a993479e009b23
Kelvin-Mutinda <mwendwamutinda24@gmail.com>
2026-08-14 09:21:22 +0300

    Update login.jsx

--- Frontend/trialbal/src/auth/login.jsx
+++ Frontend/trialbal/src/auth/login.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FaBuildingColumns, FaEnvelope, FaLock, FaArrowRightToBracket } from "react-icons/fa6";
 
+const API_URL = "https://trialbal-1.onrender.com";
+
 export default function Login() {
   const [formData, setFormData] = useState({ email: "", password: "" });
   const [status, setStatus] = useState({ type: "", text: "" });
@@ -18,7 +20,7 @@ export default function Login() {
     setLoading(true);
 
     try {
-      const res = await fetch(`${import.meta.env.VITE_API_URL}/login`, {
+      const res = await fetch(`${API_URL}/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
@@ -32,10 +34,10 @@ export default function Login() {
 
         setStatus({ type: "success", text: "Login successful! Redirecting…" });
 
-       setTimeout(() => {
-  console.log("Attempting navigate to /home");
-  navigate("/home");
-}, 1000);
+        setTimeout(() => {
+          console.log("Attempting navigate to /home");
+          navigate("/home");
+        }, 1000);
       } else {
         setStatus({
           type: "error",
